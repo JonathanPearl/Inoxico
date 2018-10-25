830b2d806a2e25f06dcfe5f5ffa093d907796ebc
Jonathan <jonathan@maximasoftware.co.za>
2018-10-25 09:58:36 +0200

    Counter Pit Example Made

--- PitTests/TestDeepestPit.cs
+++ PitTests/TestDeepestPit.cs
@@ -13,5 +13,21 @@ namespace PitTests
         {
             Assert.True(Inoxico.TechnicalTest.DeepestPitAnswer.IsLocationPit(locationVaues));
         }
+
+        [Theory]
+        [InlineData(new int[] { 3, 5, 0 })]
+        [InlineData(new int[] { 1, 3, 2 })]
+        public void LocationsAreNotPits(int[] locationVaues)
+        {
+            Assert.False(Inoxico.TechnicalTest.DeepestPitAnswer.IsLocationPit(locationVaues));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 3, -2, 0 })]
+        [InlineData(new int[] { 1, -3, 2 })]
+        public void PitDepth(int[] locationVaues)
+        {
+            Assert.True(Inoxico.TechnicalTest.DeepestPitAnswer.IsLocationPit(locationVaues));
+        }
     }
 }
